98588e33bac179cb2da116cb4bad5d0f3614a4be
stjchang <changstephen289@gmail.com>
2025-11-03 03:21:51 -0500

    created directories for mongodb and postgresql for seperate implementations and a general class interface databasemanager

---
+++ server/db/DatabaseManager.js
@@ -0,0 +1,51 @@
+class DatabaseManager {
+    async initialize() {
+        throw new Error("initialize() not implemented");
+    }
+
+    async close() {
+        throw new Error("close() not implemented");
+    }
+
+    //users
+
+    async getUserByEmail(email) {
+        throw new Error("getUserByEmail() not implemented");
+    }
+
+    async createUser(userObject) {
+        throw new Error("createUser() not implemented");
+    }
+
+    async updateUserById(id, userObject) {
+        throw new Error("updateUserById() not implemented");
+    }
+
+    async deleteUserById(id) {
+        throw new Error("deleteUserById() not implemented");
+    }
+
+    //playlists
+
+    async createPlaylist(playlistObject) {
+        throw new Error("createPlaylist() not implemented");
+    }
+
+    async deletePlaylistById(id) {
+        throw new Error("deletePlaylistById() not implemented");
+    }
+
+    async getPlaylistById(id) { 
+        throw new Error("getPlaylistById() not implemented");
+    }
+
+    async getPlaylistPairs() {
+        throw new Error("getPlaylistPairs() not implemented");
+    }
+
+    async updatePlaylistById(id, playlistObject) {
+        throw new Error("updatePlaylistById() not implemented");
+    }
+}
+
+module.exports = DatabaseManager;
